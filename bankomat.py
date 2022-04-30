e5b0a8273605518bd7a3bbaba59b5c610a048a2e
sqwlasss <97263903+sqwlasss@users.noreply.github.com>
2022-04-30 17:26:43 +0300

    Update bankomat.py

--- bankomat.py
+++ bankomat.py
@@ -1,19 +1,83 @@
-person = {"name" : "cristy", "money":777, "PIN":"1"}
+person = {"name": "cristy", "money": 777, "PIN": "1"}
 PIN = 2001
-
-
-
-
 a = input("write your name please:")
 b = input("PIN:")
-#oil = input("Пожалуйста, введите выбор, который вы хотите сделать:"
- #      "\n1)Откройте счет\n2)Aвторизация\n3)Выход;")
-#z = ("Откройте счет")
+# oil = input("Пожалуйста, введите выбор, который вы хотите сделать:"
+#      "\n1)Откройте счет\n2)Aвторизация\n3)Выход;")
+# z = ("Откройте счет")
 key = ""
-key = input()
 print("Aвторизация, key ""s")
 print("Выход, key ""f""'")
-if key == 's':
-       name = input('your name is')
-#if oil != "D":
+name = input('chose your option pls  ')
+if name == 's':
+    name = input('your name is')
+    print(name)
+elif key != "s, f":
+    pass
+if name != 'cristy':
+    print("try again please")
+def fun1(qwerty):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if oil != "D":
 #    print("oo")
