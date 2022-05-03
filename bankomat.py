c9e7112ad4ce7263f5200bedfdb91352dff18b9f
sqwlasss <scebec.cristian@gmail.com>
2022-05-03 18:55:21 +0300

    2 change

--- bankomat.py
+++ bankomat.py
@@ -1,3 +1,8 @@
+dictionary = {
+   'name1' : 'cristy',
+   'PIN' : '2001',
+   'money' : 777
+}
 person = {"name": "cristy", "money": 777, "PIN": "1"}
 PIN = 2001
 a = input("write your name please:")
@@ -10,14 +15,24 @@ print("Aвторизация, key ""s")
 print("Выход, key ""f""'")
 name = input('chose your option pls  ')
 if name == 's':
-    name = input('your name is')
+    name = input('your name is  ')
     print(name)
 elif key != "s, f":
     pass
 if name != 'cristy':
     print("try again please")
+if name == 'cristy':
+ if name == 's':
+    name = input('input you PIN ')
+    print(name + 'Счет успешно открыт!')
+def fun2(password):
+    if PIN != 2001:
+     abc = input('press enter to return')
+
+    return
 def fun1(qwerty):
     pass
+
 
 
 
